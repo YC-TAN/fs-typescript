8f32817bc48232cd343f02f155100fdcec6fc14d
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-17 22:01:07 +1200

    Exercise command line

--- healthapp/exerciseCalculator.ts
+++ healthapp/exerciseCalculator.ts
@@ -8,6 +8,26 @@ interface Result {
     average: number
 }
 
+interface argValues {
+    value1: number[],
+    value2: number
+}
+
+const parseArguments = (args: string[]): argValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+    const [, , ...rest] = args
+    const checked = rest.map(Number)
+    if (checked.some(isNaN)) {
+        throw new Error('Provided values were not numbers!');
+    }
+    const value1 = checked.splice(0, checked.length - 1)
+    const value2 = checked[checked.length - 1]
+    return {
+        value1,
+        value2
+    }
+}
+
 const description: Record<number, string> = {
   1: "Not enough",
   2: "Good",
@@ -17,7 +37,7 @@ const description: Record<number, string> = {
 const calculateExercises = (exerciseHrs: number[], target: number): Result => {
     const periodLength = exerciseHrs.length
     const trainingDays = exerciseHrs.filter(d => d > 0).length
-    if (periodLength <=0 ) throw new Error('no training record found')
+    if (periodLength <= 0 ) throw new Error('no training record found')
     const average = exerciseHrs.reduce((sum, item) => sum + item, 0)/periodLength
 
     let rating: 1 | 2 | 3;
@@ -40,4 +60,15 @@ const calculateExercises = (exerciseHrs: number[], target: number): Result => {
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+try {
+    const { value1, value2 } = parseArguments(process.argv as string[])
+    console.log(calculateExercises(value1, value2))
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
