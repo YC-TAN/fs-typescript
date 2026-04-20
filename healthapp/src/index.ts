61ed720bbb53d2e549582647ab37cf0aeff70729
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-20 15:49:05 +1200

    Implement GET /bmi endpoint

--- healthapp/src/index.ts
+++ healthapp/src/index.ts
@@ -1,9 +1,41 @@
-import express from 'express'
+import express from 'express';
+import { calculateBmi } from './bmi/bmiCalculator.ts';
+import isNotNumber from './bmi/utils.ts';
 
 const app = express()
 
 app.get('/hello', (_req, res) => {
-    return res.send("Hello Full Stack!")
+    return res.send("Hello Full Stack!");
+})
+
+app.get('/bmi', (req, res) => {
+    const { height=0, weight=0 } = req.query;
+
+    if (!height || !weight) {
+        return res.status(400).json({ error: "parameters missing" });
+    }
+
+    if (isNotNumber(height) || isNotNumber(weight)) {
+        return res.status(400).json({error: "malformatted parameters"})
+    }
+
+    const heightInCM = Number(height)
+    const weightInKG = Number(weight)
+    try {
+        const bmi = calculateBmi(heightInCM, weightInKG);
+        return res.json({
+            weight: weightInKG, 
+            height: heightInCM, 
+            bmi: bmi
+        })
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return res.status(400).json({error: error.message})
+        } else {
+            return res.status(500).json({error: "Something went wrong..."})
+        }
+    }
+    
 })
 
 const PORT = 3003
