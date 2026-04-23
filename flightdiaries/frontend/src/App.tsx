a7ebee42ed85882e1590dfff90d7aa171177127d
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-23 15:16:27 +1200

    Implement add new diary service

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -1,22 +1,29 @@
 import { useEffect, useState } from 'react'
 
-import type { DiaryEntry } from './types'
-import flightService from './services/flightService'
+import type { DiaryEntry, NewDiaryEntry } from './types'
+import diaryService from './services/diaryService'
 import DiaryList from './components/DiaryList'
+import DiaryForm from './components/DiaryForm'
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
 
   useEffect(() => {
     const initialDiaries = async () => {
-      const data = await flightService.getAll()
+      const data = await diaryService.getAll()
       setDiaries(data)    
     }
     void initialDiaries()
   }, [])
 
+  const addDiary = async (newDiary: NewDiaryEntry) => {
+    const addedDiary = await diaryService.create(newDiary)
+    setDiaries(prev => [...prev, addedDiary])
+  }
+
   return (
     <>
+    <DiaryForm addDiary={addDiary} />
       <DiaryList diaries={diaries} />
     </>
   )
