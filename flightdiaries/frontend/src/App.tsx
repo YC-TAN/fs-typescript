1e916154636b93cf981157733da1c0b2a1cf0d78
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-23 16:13:32 +1200

    Implement error handling

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-import type { DiaryEntry, NewDiaryEntry } from './types'
+import type { DiaryEntry, DiaryFormValues } from './types'
 import diaryService from './services/diaryService'
 import DiaryList from './components/DiaryList'
 import DiaryForm from './components/DiaryForm'
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+  const [message, setMessage] = useState<string | null>(null)
 
   useEffect(() => {
     const initialDiaries = async () => {
@@ -16,14 +18,27 @@ const App = () => {
     void initialDiaries()
   }, [])
 
-  const addDiary = async (newDiary: NewDiaryEntry) => {
-    const addedDiary = await diaryService.create(newDiary)
-    setDiaries(prev => [...prev, addedDiary])
+  const addDiary = async (newDiary: DiaryFormValues) => {
+    try {
+      const addedDiary = await diaryService.create(newDiary)
+      setDiaries(prev => [...prev, addedDiary])
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        if (error?.response?.data?.error[0]?.message && typeof error?.response?.data?.error[0]?.message === 'string') {
+          console.log(error.response.data.error[0].message)
+          setMessage(`Error: ${error.response.data.error[0].message}`)
+        }      
+    } else {
+      console.error("Unknown error", error);
+        setMessage(`Error: something went wrong...`)
+      }
+    }
   }
 
   return (
     <>
-    <DiaryForm addDiary={addDiary} />
+      {message && <div>{message}</div>}
+      <DiaryForm addDiary={addDiary} />
       <DiaryList diaries={diaries} />
     </>
   )
