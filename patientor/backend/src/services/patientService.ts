f274f1278fad31fcd57d772dcd46b02ad8c77e8c
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-24 20:07:07 +1200

    Add getById service

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,10 +1,10 @@
-import type {PatientWithoutSsn, NewPatientEntry, Patient} from '../types.ts';
+import type {NonSensitivePatient, NewPatientEntry, Patient} from '../types.ts';
 import patients from '../data/patients.ts';
 import { v4 as uuid } from 'uuid';
 
 
-const getAllPatientsWithoutSsn = (): PatientWithoutSsn[] => {
-    return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
+const getAllPatientsWithoutSsn = (): NonSensitivePatient[] => {
+    return patients.map(({id, name, dateOfBirth, gender, occupation, }) => ({
         id,
         name,
         dateOfBirth,
@@ -13,11 +13,16 @@ const getAllPatientsWithoutSsn = (): PatientWithoutSsn[] => {
     }));
 };
 
+const getById = (id: string): Patient | undefined => {
+    return patients.find(p => p.id === id)
+}
+
 const create = (newPatient: NewPatientEntry): Patient => {
     const id: string = uuid();
     const patient = {
         ...newPatient,
-        id
+        id,
+        entries: []
     };
     patients.push(patient);
     return patient;
@@ -25,5 +30,6 @@ const create = (newPatient: NewPatientEntry): Patient => {
 
 export default {
     getAllPatientsWithoutSsn,
-    create
+    create,
+    getById,
 };
