66f5f8ceac4c58439a9728f410fc159b32f29386
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-27 15:54:24 +1200

    Implement Entry type

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -6,6 +6,8 @@ export const Gender = {
     Other: 'other'
 } as const;
 
+export type Gender = typeof Gender[keyof typeof Gender];
+
 export const NewPatientSchema = z.object({
     name: z.string(),
     ssn: z.string(),
@@ -20,10 +22,55 @@ export interface Diagnosis {
     latin?: string
 };
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
+const HealthCheckRating = {
+    Healthy: 0,
+    LowRisk: 1,
+    HighRisk: 2,
+    CriticalRisk: 3
+} as const;
+
+export type HealthCheckRating = typeof HealthCheckRating[keyof typeof HealthCheckRating];
+
+interface SickLeave {
+    startDate: string,
+    endDate: string
 }
 
+interface Discharge {
+    date: string,
+    criteria: string,
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: Discharge;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export type NewPatientEntry = z.infer<typeof NewPatientSchema>;
 export interface Patient {
   id: string;
   name: string;
@@ -33,12 +80,9 @@ export interface Patient {
   dateOfBirth: string;
   entries: Entry[]
 }
-
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
-export type Gender = typeof Gender[keyof typeof Gender];
-export type NewPatientEntry = z.infer<typeof NewPatientSchema>;
-// export interface Patient extends NewPatientEntry {
-//     id: string
-// };
-// export type PatientWithoutSsn = Omit<Patient, 'ssn'>;
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
