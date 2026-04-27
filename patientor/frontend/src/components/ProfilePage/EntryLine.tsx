44f27f86de24a233abadd78e1368d4aa5aa31cdc
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-28 09:38:08 +1200

    Add switch/case render for discriminated union type

--- patientor/frontend/src/components/ProfilePage/EntryLine.tsx
+++ patientor/frontend/src/components/ProfilePage/EntryLine.tsx
@@ -1,4 +1,7 @@
 import { Typography } from "@mui/material";
+import {WorkOutlined} from '@mui/icons-material';
+import {MedicalServices} from '@mui/icons-material';
+import {LocalHospital} from '@mui/icons-material';
 import type { Entry, Diagnosis } from "../../types";
 
 const assertNever = (value: never): never => {
@@ -7,9 +10,83 @@ const assertNever = (value: never): never => {
     );
 };
 
-interface EntryProps {
+interface BaseProps {
+    diagnoses: Diagnosis[] | undefined;
+}
+
+interface DiagnosisListProps extends BaseProps {
+    diagnosisCodes: string[] | undefined;
+}
+
+interface HealthCheckEntryProps extends BaseProps {
+    entry: HealthCheckEntry;
+}
+
+
+interface HospitalEntryProps extends BaseProps {
+    entry: HospitalEntry;
+}
+
+interface OccupationalHealthcareEntryProps extends BaseProps {
+    entry: OccupationalHealthcareEntry;
+}
+
+interface EntryProps extends BaseProps {
     entry: Entry | undefined | null;
-    diagnoses: Diagnosis[];
+}
+
+const DiagnosisList = ({diagnosisCodes, diagnoses}: DiagnosisListProps) => {
+    if (!diagnosisCodes || diagnosisCodes.length === 0) return null;
+    return (
+        <ul>
+            { diagnosisCodes.map(c => (
+                <li key={c}>
+                    <Typography variant="body2">{c} {diagnoses?.find(d => d.code === c)?.name}</Typography>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const HealthCheck = ({entry, diagnoses} : HealthCheckEntryProps) => {
+    return (
+        <>
+            <Typography variant='body2'>
+                {entry.date} <MedicalServices />
+            </Typography>
+            <Typography variant="body2"><i>{entry.description}</i></Typography>
+            <Typography>{entry.healthCheckRating}</Typography>
+            <DiagnosisList diagnoses={diagnoses} diagnosisCodes={entry.diagnosisCodes} />                        
+        </>
+    )
+}
+
+const Hospital = ({entry, diagnoses} : HospitalEntryProps) => {
+    return (
+        <>
+            <Typography variant='body2'>
+                {entry.date} <LocalHospital />
+            </Typography>
+            <Typography variant="body2"><i>{entry.description}</i></Typography>
+            <DiagnosisList diagnoses={diagnoses} diagnosisCodes={entry.diagnosisCodes} />
+            <Typography variant="body2">Discharge: {entry.discharge.date} {entry.discharge.criteria}</Typography>
+            
+        </>
+    )
+}
+
+const OccupationalHealthcare = ({entry, diagnoses} : OccupationalHealthcareEntryProps) => {
+    return (
+        <>
+            <Typography variant='body2'>
+                {entry.date} <WorkOutlined /> {entry.employerName}
+            </Typography>
+            <Typography variant="body2"><i>{entry.description}</i></Typography>
+            <DiagnosisList diagnoses={diagnoses} diagnosisCodes={entry.diagnosisCodes} />
+            <Typography variant="body2">{entry.sickLeave && `Sick Leave: ${entry.sickLeave?.startDate} - ${entry.sickLeave?.endDate}`}</Typography>
+            
+        </>
+    )
 }
 
 const EntryLine = ({entry, diagnoses} : EntryProps) => {
@@ -18,40 +95,27 @@ const EntryLine = ({entry, diagnoses} : EntryProps) => {
     const renderEntry = () => {
         switch (entry.type) {
             case 'HealthCheck': 
-                return (
-                    <>
-                    <p></p>
-                    </>
-                );
+                return (<HealthCheck entry={entry} diagnoses={diagnoses} />);
             case "Hospital":
-                return (
-                    <>
-                    <p></p>
-                    </>
-                );
+                return (<Hospital entry={entry} diagnoses={diagnoses} />);
             case "OccupationalHealthcare":
-                return (
-                    <>
-                    <p></p>
-                    </>
-                );
+                return (<OccupationalHealthcare entry={entry} diagnoses={diagnoses} />);
             default:
                 return assertNever(entry);
         }   
     };
-  return (
-    <div>
-        <Typography variant='body2'>{entry.date} <i>{entry.description}</i></Typography>
-        <ul>
-            { entry.diagnosisCodes?.map(c => (
-                <li key={c}>
-                    <Typography variant="body2">{c} {diagnoses?.find(d => d.code === c)?.name}</Typography>
-                    </li>
-            ))}
-        </ul>
-        {renderEntry()}
-    </div>
-  );
+
+    return (
+        <div style={{
+            border: '1px solid black',
+            borderRadius: '0.25rem',
+            padding: '2px',
+            margin: '10px 0'
+        }}>
+            {renderEntry()}
+            <Typography variant='body2'>diagnosed by {entry.specialist}</Typography>
+        </div>
+    );
 };
 
 export default EntryLine;
