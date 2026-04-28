b538efdcf4dd6954224393e641c58938f80cb944
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-28 13:20:18 +1200

    Implement POST /api/patients/:id/entries

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,7 +1,7 @@
 import express, {type Request, type Response} from 'express';
 import patientService from '../services/patientService.ts';
-import type { Patient, NewPatientEntry } from '../types.ts';
-import { newPatientParser } from '../middleware.ts';
+import type { Patient, NewPatientEntry, EntryWithoutId, Entry } from '../types.ts';
+import { newPatientParser, newEntryParser } from '../middleware.ts';
 
 const router = express.Router();
 
@@ -26,6 +26,18 @@ router.get('/:id', (req, res: Response<Patient | {error: string}>) => {
 router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatientEntry>, res: Response<Patient>) => {
     const addedPatient = patientService.create(req.body);
     return res.json(addedPatient);
+});
+
+router.post('/:id/entries', newEntryParser, (req: Request<{id: string}, unknown, EntryWithoutId>, res: Response<Entry | {error: string}>) => {
+    try {
+        const addedEntry = patientService.createEntry(req.params.id, req.body);
+        return res.json(addedEntry);
+    } catch (e) {
+        if (e instanceof Error) {
+            return res.status(404).json({ error: e.message });
+        }
+        return res.status(500).json({ error: 'Unknown error' });
+    }
 });
 
 export default router;
