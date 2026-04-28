1eb7d90a31bda594e0f764c0edda10dc650df012
YC-TAN <150326068+YC-TAN@users.noreply.github.com.>
2026-04-28 13:18:36 +1200

    Add newEntryParser middleware

--- patientor/backend/src/middleware.ts
+++ patientor/backend/src/middleware.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response, type NextFunction } from 'express';
-import { NewPatientSchema } from './types.ts';
+import { NewPatientSchema, NewEntrySchema } from './types.ts';
 import {z} from 'zod';
 
 export const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
@@ -9,6 +9,15 @@ export const newPatientParser = (req: Request, _res: Response, next: NextFunctio
     } catch (error: unknown) {
         next(error);
     }
+};
+
+export const newEntryParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    NewEntrySchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
 };
 
 export const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
